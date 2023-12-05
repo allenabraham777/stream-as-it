04a4f02167d7bfeb5f17771046a55b37bfd20ee4
allenabraham777 <allenabraham777@gmail.com>
2023-12-05 22:23:01 +0530

    refactor: Change database schema

---
+++ packages/db/prisma/migrations/20231205153813_init/migration.sql
@@ -0,0 +1,69 @@
+-- CreateTable
+CREATE TABLE "Account" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "account_name" TEXT NOT NULL,
+    "created_at" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" DATETIME NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "account_id" INTEGER NOT NULL,
+    "username" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "email_verified" BOOLEAN NOT NULL DEFAULT false,
+    "verification_token" TEXT,
+    "reset_token" TEXT,
+    "created_at" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" DATETIME NOT NULL,
+    CONSTRAINT "User_account_id_fkey" FOREIGN KEY ("account_id") REFERENCES "Account" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+
+-- CreateTable
+CREATE TABLE "Stream" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "user_id" INTEGER NOT NULL,
+    "account_id" INTEGER NOT NULL,
+    "stream_title" TEXT NOT NULL,
+    "stream_description" TEXT NOT NULL,
+    "is_live" BOOLEAN NOT NULL,
+    "created_at" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" DATETIME NOT NULL,
+    CONSTRAINT "Stream_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "User" ("id") ON DELETE RESTRICT ON UPDATE CASCADE,
+    CONSTRAINT "Stream_account_id_fkey" FOREIGN KEY ("account_id") REFERENCES "Account" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+
+-- CreateTable
+CREATE TABLE "StreamKey" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "stream_id" INTEGER NOT NULL,
+    "platform" TEXT NOT NULL,
+    "stream_key" TEXT NOT NULL,
+    "account_id" INTEGER NOT NULL,
+    "created_at" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" DATETIME NOT NULL,
+    CONSTRAINT "StreamKey_stream_id_fkey" FOREIGN KEY ("stream_id") REFERENCES "Stream" ("id") ON DELETE RESTRICT ON UPDATE CASCADE,
+    CONSTRAINT "StreamKey_account_id_fkey" FOREIGN KEY ("account_id") REFERENCES "Account" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+
+-- CreateTable
+CREATE TABLE "ForgotPassword" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "user_id" INTEGER NOT NULL,
+    "account_id" INTEGER NOT NULL,
+    "token" TEXT NOT NULL,
+    "expires_at" DATETIME NOT NULL,
+    CONSTRAINT "ForgotPassword_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "User" ("id") ON DELETE RESTRICT ON UPDATE CASCADE,
+    CONSTRAINT "ForgotPassword_account_id_fkey" FOREIGN KEY ("account_id") REFERENCES "Account" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Account_account_name_key" ON "Account"("account_name");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "StreamKey_stream_key_key" ON "StreamKey"("stream_key");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "ForgotPassword_user_id_key" ON "ForgotPassword"("user_id");
