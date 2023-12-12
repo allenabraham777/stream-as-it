97bdcbc838107408b86a3ec592bf8d71419697c4
allenabraham777 <allenabraham777@gmail.com>
2023-12-13 01:45:26 +0530

    refactor(package-ui): tweak theme toggle

--- packages/ui/src/components/custom/ThemeToggle.tsx
+++ packages/ui/src/components/custom/ThemeToggle.tsx
@@ -18,7 +18,11 @@ function ThemeToggle({ setTheme }: props) {
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
-        <Button variant="outline" size="icon">
+        <Button
+          variant="outline"
+          size="icon"
+          className="bg-yellow-400 dark:bg-background  rounded-full"
+        >
           <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
           <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
           <span className="sr-only">Toggle theme</span>
