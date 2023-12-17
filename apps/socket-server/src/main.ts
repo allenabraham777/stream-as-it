b1cb1cc6291e9d541c1d52f2057c10ac6f26b452
allenabraham777 <allenabraham777@gmail.com>
2023-12-17 22:54:25 +0530

    refactor(apps): adjust ports

--- apps/socket-server/src/main.ts
+++ apps/socket-server/src/main.ts
@@ -6,6 +6,6 @@ async function bootstrap() {
     const app = await NestFactory.create(AppModule);
     app.enableCors();
     app.useWebSocketAdapter(new IoAdapter(app));
-    await app.listen(3002);
+    await app.listen(8004);
 }
 bootstrap();
