d97f05ce055b54f14677773fd4697cd649e63c85
allenabraham777 <allenabraham777@gmail.com>
2024-01-03 02:18:20 +0530

    feat(stream-server): add health check module

--- apps/stream-server/src/app.module.ts
+++ apps/stream-server/src/app.module.ts
@@ -2,14 +2,14 @@ import { Module } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { ConfigModule } from '@nestjs/config';
 
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { HealthModule } from '@stream-as-it/health';
+
 import { TwitchGateway } from './stream/twitch.gateway';
 import { YoutubeGateway } from './stream/youtube.gateway';
 
 @Module({
-    imports: [ConfigModule.forRoot()],
-    controllers: [AppController],
-    providers: [AppService, JwtService, YoutubeGateway, TwitchGateway]
+    imports: [ConfigModule.forRoot(), HealthModule],
+    controllers: [],
+    providers: [JwtService, YoutubeGateway, TwitchGateway]
 })
 export class AppModule {}
