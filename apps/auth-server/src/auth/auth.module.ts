a2383da0652b315906267b912b1be413849e51f5
allenabraham777 <allenabraham777@gmail.com>
2024-01-03 01:34:18 +0530

    refactor(auth-server): few tweaks in auth module to support updated passport module

--- apps/auth-server/src/auth/auth.module.ts
+++ apps/auth-server/src/auth/auth.module.ts
@@ -3,7 +3,7 @@ import { JwtModule } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
 
 import { MailService } from '@stream-as-it/email';
-import { JwtStrategy, RefreshStrategy } from '@stream-as-it/passport';
+import { PassportStrategyModule } from '@stream-as-it/passport';
 import { DatabaseModule } from '@stream-as-it/dao';
 
 import { AuthService } from './auth.service';
@@ -12,15 +12,14 @@ import { UsersRepository } from './repository/user.repository';
 import { AccountRepository } from './repository/account.repository';
 
 @Module({
-    imports: [PassportModule, JwtModule.register({}), DatabaseModule, DatabaseModule.forFeature()],
+    imports: [
+        PassportModule,
+        JwtModule.register({}),
+        DatabaseModule,
+        DatabaseModule.forFeature(),
+        PassportStrategyModule
+    ],
     controllers: [AuthController],
-    providers: [
-        AuthService,
-        JwtStrategy,
-        RefreshStrategy,
-        MailService,
-        UsersRepository,
-        AccountRepository
-    ]
+    providers: [AuthService, MailService, UsersRepository, AccountRepository]
 })
 export class AuthModule {}
