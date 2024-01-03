5df0337551f29fa050ab59d381c6d3d3be463cb6
allenabraham777 <allenabraham777@gmail.com>
2024-01-04 00:27:03 +0530

    chore(package-health): add api tag for health route

--- packages/health/src/health.controller.ts
+++ packages/health/src/health.controller.ts
@@ -1,6 +1,8 @@
 import { Controller, Get } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
 
 @Controller('/')
+@ApiTags('health')
 export class HealthController {
     @Get()
     health() {
